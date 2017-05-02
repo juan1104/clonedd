ab838bf27124cd97fe861c94443d445a196711ae
Kim JinHo <kimjinho585@gmail.com>
2017-05-02 06:37:38 +0000

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,14 @@
 Rails.application.routes.draw do
-  # get    '/posts'  => 'posts#index'
-  # post  '/posts' =>   'posts#create'
+  get    '/posts'  => 'posts#index'
+  post  '/posts' =>   'posts#create'
 
-  # get    '/posts/new' =>  'posts#new', as: 'new_post'
-
-
-  # get    '/posts/:id'   =>   'posts#show', as: 'post'
-
-  # get    '/posts/:id/edit' => 'posts#edit', as: 'edit_post'
-  # patch  '/posts/:id'   =>   'posts#update'
-  # delete '/posts/:id'   =>   'posts#destroy'
-  
-  resources 'posts' do
-    resources 'comments', only: [:create]
-  end
+  get    '/posts/new' =>  'posts#new', as: 'new_post'
 
 
+  get    '/posts/:id'   =>   'posts#show', as: 'post'
+
+  get    '/posts/:id/edit' => 'posts#edit', as: 'edit_post'
+  patch  '/posts/:id'   =>   'posts#update'
+  delete '/posts/:id'   =>   'posts#destroy'
 
 end
